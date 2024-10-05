8a0fad4a4b1c8dfb417f4bcc93d575a74652a2cb
jeanp00 <semijeanp@gmail.com>
2024-10-05 11:36:32 -0500

    Se crea el proyecto de ejemplo, para aprender C#

---
+++ FundamentosCsharp/Proyecto1/Modelos/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+
+namespace Proyecto1.Modelos
+{
+    public class Car
+    {
+        /*
+         * Atributos del carro
+         */
+        string color;
+        int cilindraje;
+        int modelo;
+
+        public string stop()
+        {
+            string msj = "El carro esta detenido";
+            return msj;
+        }
+    }
+}
